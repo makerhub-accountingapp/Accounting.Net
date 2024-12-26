333073ee85e78b2ba8207f0ec8c767b163e4289e
Yuki <yuki.marchal1216@gmail.com>
2024-12-26 05:04:32 +0100

    working on EntityTypeConfig

--- AccountingApp.DB/Contexts/UserContext.cs
+++ AccountingApp.DB/Contexts/UserContext.cs
@@ -1,50 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using AccountingApp.DB.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AccountingApp.DAL.Contexts
 {
-    public class UserContext : DbContext
+    public class UserContext : IEntityTypeConfiguration<User>
     {
         public DbSet<User> Users { get; set; } = null!;
-        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public void Configure(EntityTypeBuilder<User> builder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=formation;Database=MakerHub;Trust Server Certificate=True");
-        }
+            //! \"\" => CaseSensitive
+            builder.ToTable("\"User\"");
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // \"\" => CaseSensitive
-            modelBuilder.Entity<User>().ToTable("\"User\"");
-            
             // Properties.IsRequired
-            
-            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired();
-            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired();
 
+            builder.Property(u => u.Email).IsRequired();
+            builder.Property(u => u.Password).IsRequired();
+            
             // Properties.Others
 
-            modelBuilder.Entity<User>().Property(u => u.IsActive).HasDefaultValue(true);
+            builder.Property(u => u.IsActive).HasDefaultValue(true);
 
             // Keys
 
-            modelBuilder.Entity<User>().HasKey(u => u.Id);
-            modelBuilder.Entity<User>().HasMany(u => u.Accounts)
+            builder.HasKey(u => u.Id);
+            builder.HasMany(u => u.Accounts)
                 .WithOne(a => a.User)
                 .HasForeignKey(a => a.UserId);
 
 
             /********************** NOTE **********************/
             // If the primary key name is Id, the methods below are not necessary.
-            //modelBuilder.Entity<User>().Property(u => u.Id).ValueGeneratedOnAdd();
+            // builder.Property(u => u.Id).ValueGeneratedOnAdd();
 
-            //modelBuilder.Entity<User>().HasMany(u => u.Accounts)
+            // builder.HasMany(u => u.Accounts)
             //    .WithOne(a => a.User)
             //    .HasForeignKey(a => a.UserId)
             //    .HasPrincipalKey(u => u.Id);
