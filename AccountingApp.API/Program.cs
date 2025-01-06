b95446bac43b85f38bd2c4b467878eef93ea34d3
Yuki <yuki.marchal1216@gmail.com>
2025-01-06 09:34:38 +0100

    Detail includes Transaction ok

--- AccountingApp.API/Program.cs
+++ AccountingApp.API/Program.cs
@@ -20,7 +20,11 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IDetailRepository, DetailRepository>();
 builder.Services.AddScoped<IDetailService, DetailService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler =
+        System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- AccountingApp.DAL/Repositories/DetailRepository.cs
+++ AccountingApp.DAL/Repositories/DetailRepository.cs
@@ -7,10 +7,22 @@ using AccountingApp.DAL.Interfaces;
 using AccountingApp.DB.Contexts;
 using AccountingApp.DB.Entities;
 using AccountingApp.Tools.Templates;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccountingApp.DAL.Repositories
 {
     public class DetailRepository(AccountingContext context) : Repository<Detail>(context), IDetailRepository
     {
+        public override IEnumerable<Detail> Get(Func<Detail, bool>? predicate)
+        {
+            if (predicate is not null) 
+                return Entities.Include(d => d.Transaction).Where(predicate);
+            else 
+                return Entities.Include(d => d.Transaction);
+        }
+        public override Detail? GetOne(Func<Detail, bool> predicate)
+        {
+            return Entities.Include(d => d.Transaction).FirstOrDefault(predicate);
+        }
     }
 }
